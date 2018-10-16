5c2decf9654ce1fb99e032448a39747cf43bd9fb
Ubuntu <ubuntu@ip-172-31-28-206.eu-west-1.compute.internal>
2018-10-16 17:21:22 +0000

    script with nohup

---
+++ start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd /home/ubuntu/IoT-data-platform/python && nohup python mqtt_to_json.py > /data/logs/mqtt_to_json.out 2>&1 &
+
+export JUPYTER_PATH=/data
+cd /home/ubuntu/IoT-data-platform/notebooks && nohup jupyter notebook > /data/logs/jupyter.out 2>&1 &
